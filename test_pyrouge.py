eabbf325f0c1f4f0a798c375f44508082a7c9bed
Li-Ming-Fan <li_m_f@163.com>
2018-12-22 22:13:53 +0800

    new file:   ROUGE-1.5.5/README.txt 	new file:   ROUGE-1.5.5/RELEASE-NOTE.txt 	new file:   ROUGE-1.5.5/ROUGE-1.5.5.pl 	new file:   ROUGE-1.5.5/XML/DOM.pm 	new file:   ROUGE-1.5.5/XML/DOM/AttDef.pod 	new file:   ROUGE-1.5.5/XML/DOM/AttlistDecl.pod 	new file:   ROUGE-1.5.5/XML/DOM/Attr.pod 	new file:   ROUGE-1.5.5/XML/DOM/CDATASection.pod 	new file:   ROUGE-1.5.5/XML/DOM/CharacterData.pod 	new file:   ROUGE-1.5.5/XML/DOM/Comment.pod 	new file:   ROUGE-1.5.5/XML/DOM/DOMException.pm 	new file:   ROUGE-1.5.5/XML/DOM/DOMImplementation.pod 	new file:   ROUGE-1.5.5/XML/DOM/Document.pod 	new file:   ROUGE-1.5.5/XML/DOM/DocumentFragment.pod 	new file:   ROUGE-1.5.5/XML/DOM/DocumentType.pod 	new file:   ROUGE-1.5.5/XML/DOM/Element.pod 	new file:   ROUGE-1.5.5/XML/DOM/ElementDecl.pod 	new file:   ROUGE-1.5.5/XML/DOM/Entity.pod 	new file:   ROUGE-1.5.5/XML/DOM/EntityReference.pod 	new file:   ROUGE-1.5.5/XML/DOM/NamedNodeMap.pm 	new file:   ROUGE-1.5.5/XML/DOM/NamedNodeMap.pod 	new file:   ROUGE-1.5.5/XML/DOM/Node.pod 	new file:   ROUGE-1.5.5/XML/DOM/NodeList.pm 	new file:   ROUGE-1.5.5/XML/DOM/NodeList.pod 	new file:   ROUGE-1.5.5/XML/DOM/Notation.pod 	new file:   ROUGE-1.5.5/XML/DOM/Parser.pod 	new file:   ROUGE-1.5.5/XML/DOM/PerlSAX.pm 	new file:   ROUGE-1.5.5/XML/DOM/ProcessingInstruction.pod 	new file:   ROUGE-1.5.5/XML/DOM/Text.pod 	new file:   ROUGE-1.5.5/XML/DOM/XMLDecl.pod 	new file:   ROUGE-1.5.5/XML/Handler/BuildDOM.pm 	new file:   ROUGE-1.5.5/XML/RegExp.pm 	new file:   ROUGE-1.5.5/data/WordNet-1.6-Exceptions/adj.exc 	new file:   ROUGE-1.5.5/data/WordNet-1.6-Exceptions/adv.exc 	new file:   ROUGE-1.5.5/data/WordNet-1.6-Exceptions/buildExeptionDB.pl 	new file:   ROUGE-1.5.5/data/WordNet-1.6-Exceptions/noun.exc 	new file:   ROUGE-1.5.5/data/WordNet-1.6-Exceptions/verb.exc 	new file:   ROUGE-1.5.5/data/WordNet-2.0-Exceptions/adj.exc 	new file:   ROUGE-1.5.5/data/WordNet-2.0-Exceptions/adv.exc 	new file:   ROUGE-1.5.5/data/WordNet-2.0-Exceptions/buildExeptionDB.pl 	new file:   ROUGE-1.5.5/data/WordNet-2.0-Exceptions/noun.exc         new file:   ROUGE-1.5.5/data/WordNet-2.0-Exceptions/verb.exc 	new file:   ROUGE-1.5.5/data/WordNet-2.0.exc.db 	new file:   ROUGE-1.5.5/data/smart_common_words.txt 	new file:   ROUGE-1.5.5/run_test/runROUGE-test.pl 	new file:   data/model/example.A.000001.txt 	new file:   data/model/example.A.000002.txt 	new file:   data/reference/example.000001.txt 	new file:   data/reference/example.000002.txt 	new file:   how_to_install_ROUGE.txt 	new file:   test_pyrouge.py

---
+++ test_pyrouge.py
@@ -0,0 +1,17 @@
+
+from pyrouge import Rouge155
+
+rg = Rouge155()
+rg.system_dir = './data/reference'
+rg.model_dir = './data/model'
+rg.system_filename_pattern = 'example.(\d+).txt'
+rg.model_filename_pattern = 'example.[A-Z].#ID#.txt'
+
+output = rg.convert_and_evaluate()
+print(output)
+output_dict = rg.output_to_dict(output)
+
+
+
+
+
