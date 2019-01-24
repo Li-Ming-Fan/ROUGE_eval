f06e1e98d95f240602adec965239bac13848d202
Li-Ming-Fan <li_m_f@163.com>
2019-01-24 05:41:48 -0800

    renamed:    data/model/example.A.000001.txt -> data_result/model/example.A.000001.txt 	renamed:    data/model/example.A.000002.txt -> data_result/model/example.A.000002.txt 	renamed:    data/reference/example.000001.txt -> data_result/reference/example.000001.txt 	renamed:    data/reference/example.000002.txt -> data_result/reference/example.000002.txt 	modified:   how_to_install_ROUGE.txt 	modified:   test_pyrouge.py 	deleted:    try_python_rouge.py

--- test_pyrouge.py
+++ test_pyrouge.py
@@ -1,14 +1,16 @@
 
+
 from pyrouge import Rouge155
 
 rg = Rouge155()
-rg.system_dir = './data/reference'
-rg.model_dir = './data/model'
+rg.system_dir = './data_result/reference'
+rg.model_dir = './data_result/model'
 rg.system_filename_pattern = 'example.(\d+).txt'
 rg.model_filename_pattern = 'example.[A-Z].#ID#.txt'
 
 output = rg.convert_and_evaluate()
 print(output)
+
 output_dict = rg.output_to_dict(output)
 
 
